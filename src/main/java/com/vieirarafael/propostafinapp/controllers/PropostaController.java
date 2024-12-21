9b8941011da99676ba715b56dea72d3309bd3ea4
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-21 00:20:30 -0300

    Implement Location header for Proposta creation responses
    Modified the `criarProposta` endpoint in `PropostaController` to include a `Location` header in the response using `ServletUriComponentsBuilder`. This ensures newly created resources return their URI, aligning with RESTful practices.

--- src/main/java/com/vieirarafael/propostafinapp/controllers/PropostaController.java
+++ src/main/java/com/vieirarafael/propostafinapp/controllers/PropostaController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
@@ -19,8 +23,15 @@ public class PropostaController {
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criarProposta(@RequestBody PropostaRequestDto request) {
 
-        return ResponseEntity.ok()
-                .body(propostaService.criar(request))
-                ;
+        PropostaResponseDto response = propostaService.criar(request);
+
+        var location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(response);
     }
 }
