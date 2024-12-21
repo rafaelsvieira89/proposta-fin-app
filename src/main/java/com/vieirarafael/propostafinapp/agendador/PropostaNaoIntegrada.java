96f50d8fef336e8b0da715b6b3a8b2d0ae02ed2a
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-21 16:19:43 -0300

    Enable scheduling for non-integrated proposals processing
    Added scheduled task to process non-integrated proposals at a fixed interval defined in application properties. Introduced utility method for updating proposals and enabled scheduling in the main application. This ensures proposals are periodically checked and integrated automatically.

--- src/main/java/com/vieirarafael/propostafinapp/agendador/PropostaNaoIntegrada.java
+++ src/main/java/com/vieirarafael/propostafinapp/agendador/PropostaNaoIntegrada.java
@@ -1,10 +1,15 @@
 package com.vieirarafael.propostafinapp.agendador;
 
+import com.vieirarafael.propostafinapp.entities.Proposta;
 import com.vieirarafael.propostafinapp.repositories.PropostaRepository;
 import com.vieirarafael.propostafinapp.services.NotificacaoRabbitMqService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Component
 public class PropostaNaoIntegrada {
@@ -18,16 +23,20 @@ public class PropostaNaoIntegrada {
         this.propostaRepository = propostaRepository;
         this.notificacaoRabbitMqService = notificacaoRabbitMqService;
     }
-
+    @Scheduled(fixedDelayString = "${agendador.propostanaointegrada.intervalo}", timeUnit = TimeUnit.SECONDS)
     public void buscarPropostasNaoIntegradas() {
         propostaRepository.findAllByIntegradaIsFalse().forEach(proposta -> {
             try {
                 notificacaoRabbitMqService.notificar(proposta, exchange);
-                proposta.setIntegrada(true);
-                propostaRepository.save(proposta);
+                atualizarProposta(proposta);
             } catch (RuntimeException e) {
                 log.error("Failed to notify or update proposta: {}", e.getMessage(), e);
             }
         });
+    }
+
+    private void atualizarProposta(Proposta proposta) {
+        proposta.setIntegrada(true);
+        propostaRepository.save(proposta);
     }
 }
