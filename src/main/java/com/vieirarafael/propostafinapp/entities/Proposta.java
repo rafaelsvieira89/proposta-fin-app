fd8524edd9bb1f379a65bd11dd8a1e14b422057c
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-20 22:44:18 -0300

    Add database config and initial entity classes
    Configured PostgreSQL connection properties in the application configuration file. Introduced `Usuario` and `Proposta` entity classes with relevant fields and a one-to-one relationship between them to support the application's data model.

---
+++ src/main/java/com/vieirarafael/propostafinapp/entities/Proposta.java
@@ -0,0 +1,25 @@
+package com.vieirarafael.propostafinapp.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Proposta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private boolean integrada;
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private Usuario usuario;
+}
---
+++ src/main/java/com/vieirarafael/propostafinapp/entities/Usuario.java
@@ -0,0 +1,23 @@
+package com.vieirarafael.propostafinapp.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+}
