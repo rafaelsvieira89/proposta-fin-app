90b918118b42cb1493f59f6d62251d7b25766148
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-20 23:28:27 -0300

    Add mappings to convert PropostaRequestDto to Proposta
    Added mappings for user-related fields and ignored specific properties like id, aprovada, integrada, and observacao. This ensures accurate data transformation while preventing unintended field overwrites.

--- src/main/java/com/vieirarafael/propostafinapp/mappers/PropostaMapper.java
+++ src/main/java/com/vieirarafael/propostafinapp/mappers/PropostaMapper.java
@@ -3,8 +3,18 @@ package com.vieirarafael.propostafinapp.mappers;
 import com.vieirarafael.propostafinapp.dto.PropostaRequestDto;
 import com.vieirarafael.propostafinapp.entities.Proposta;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PropostaMapper {
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aprovada", ignore = true)
+    @Mapping(target = "integrada", ignore = true)
+    @Mapping(target = "observacao", ignore = true)
     Proposta convertDtoToProposta(PropostaRequestDto propostaRequestDto);
 }
