643b4b51a33d244d9b36691fbfa499876ed258d1
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-21 00:45:31 -0300

    Remove unused URI import from PropostaController.
    The URI import was not being utilized in the PropostaController class, so it has been removed to clean up the code. This helps improve maintainability and reduces unnecessary clutter.

--- src/main/java/com/vieirarafael/propostafinapp/controllers/PropostaController.java
+++ src/main/java/com/vieirarafael/propostafinapp/controllers/PropostaController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @AllArgsConstructor
