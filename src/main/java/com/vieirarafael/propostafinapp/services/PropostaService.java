693cd5c21ff3d738118a485934de1ed54a6b2d10
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-20 23:41:54 -0300

    Add user persistence to PropostaService and enable Actuator
    Integrates `UsuarioRepository` into `PropostaService` to persist user data when creating a proposal. Also adds Spring Boot Actuator dependency in `build.gradle` for enhanced application monitoring and management.

--- src/main/java/com/vieirarafael/propostafinapp/services/PropostaService.java
+++ src/main/java/com/vieirarafael/propostafinapp/services/PropostaService.java
@@ -5,6 +5,7 @@ import com.vieirarafael.propostafinapp.dto.PropostaResponseDto;
 import com.vieirarafael.propostafinapp.entities.Proposta;
 import com.vieirarafael.propostafinapp.mappers.PropostaMapper;
 import com.vieirarafael.propostafinapp.repositories.PropostaRepository;
+import com.vieirarafael.propostafinapp.repositories.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class PropostaService {
     private final PropostaRepository propostaRepository;
+    private final UsuarioRepository usuarioRepository;
     private final PropostaMapper propostaMapper;
 
     public PropostaResponseDto criar(PropostaRequestDto propostaRequestDto) {
-        return converter(propostaRepository.save(converter(propostaRequestDto)));
+        var proposta = converter(propostaRequestDto);
+
+        return converter(
+                propostaRepository.save(
+                        proposta.setUsuario(
+                                usuarioRepository.save(
+                                        proposta.getUsuario()
+                                )
+                        )
+                )
+        );
     }
 
     private Proposta converter(PropostaRequestDto request) {
