528d049a8f455a05201e58f7f5a0310db08ea7b7
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-20 22:18:59 -0300

    Initialize Spring Boot project with Gradle setup
    Add base Spring Boot project with Gradle support, including configurations for Java toolchain, dependencies, and test setup. Includes `.gitignore`, build scripts, basic application structure, and Gradle wrapper files.

---
+++ src/main/java/com/vieirarafael/propostafinapp/PropostaFinAppApplication.java
@@ -0,0 +1,13 @@
+package com.vieirarafael.propostafinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaFinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropostaFinAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vieirarafael/propostafinapp/PropostaFinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieirarafael.propostafinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaFinAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
